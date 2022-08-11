4195b46b993ec81b9ddf4e4bc4f2ec258e6ffe33
abhishek-codes <abhishek73knp@gmail.com>
2022-08-11 18:56:29 +0530

    commit

---
+++ src/test/java/com/demoapp/DeploymentTest/DeploymentTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demoapp.DeploymentTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeploymentTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
